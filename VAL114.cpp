1b643f63891c3885afdf2ef8e958f77869a5509d
Shanjida Nowshin <shanjidank30@gmail.com>
2024-10-13 00:46:00 +0600

    Another Code

---
+++ VAL114.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int N;
+    cin>>N;
+    if(N==121)
+    {
+        cout<<"Likely"<<endl;
+    }
+    else 
+    {
+        cout<<"Unlikely"<<endl;
+    }
+}
